05373eaaf3c1443335b474c7b84793ba52b137b2
kwazart <artdd52669@gmail.com>
2021-10-12 14:09:24 +0300

    create superuser added data to the database

--- products/models.py
+++ products/models.py
@@ -8,6 +8,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):  # переопределяем метод для вывода значения в консоли в нужном нам формате
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -16,4 +19,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0) # decimal_places = сколько цифр после запятой
     quantity = models.PositiveIntegerField(default=0)  # количество не должно быть отрицательным числом
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)  # связь с таблицей ProductCategory
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'  # название | название категории
 
