80cfe655a5d09f7991de4e68d1a9a695a6749612
kwazart <artdd52669@gmail.com>
2021-10-20 13:18:58 +0300

    error correction

--- baskets/views.py
+++ baskets/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import HttpResponseRedirect
+
 from products.models import Product
 from baskets.models import Basket
 
@@ -8,7 +9,7 @@ def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)  # товар, который добавляем в корзину
     baskets = Basket.objects.filter(user=request.user, product=product)  # добавлен ли товар в корзину для данного user
 
-    if baskets.exists(): # если товара нет в корзине
+    if not baskets.exists():  # если товара нет в корзине
         Basket.objects.create(user=request.user, product=product, quantity=1)  # создаем товар для пользователя
         return HttpResponseRedirect(request.META['HTTP_REFERER'])  # возвращает на страницу где было выполнено действие
 
