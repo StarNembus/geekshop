804a47f9df7c25abdbffcd4df0244968620f2cd9
kwazart <artdd52669@gmail.com>
2021-10-22 17:59:44 +0300

    added cart functionality via ajax

--- baskets/views.py
+++ baskets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required  # для декоратора
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 from products.models import Product
 from baskets.models import Basket
@@ -29,3 +31,18 @@ def basket_remove(request, id):
     basket = Basket.objects.get(id=id)
     basket.delete()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_edit(request, id, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+    baskets = Basket.objects.filter(user=request.user)
+    context = {'baskets': baskets}
+    result = render_to_string('baskets/baskets.html', context)  # заменить шаблон html контекстом (context)
+    return JsonResponse({'result': result})   # json ответ
