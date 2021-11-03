780e96b650d5cf03fb5ad262aa02e5cba19b9003
kwazart <artdd52669@gmail.com>
2021-11-03 12:32:53 +0300

    added model activation_key

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
+from datetime import timedelta
 
 
 # Create your models here.
@@ -11,3 +13,12 @@ class User(AbstractUser):
     def safe_delete(self):
         self.is_active = False
         self.save()  # для def admin_users_delete
+
+    activation_key = models.CharField(max_length=128, blank=True)
+    activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
+
+    def is_activation_key_expired(self):  # проверка актуальности ключа(когда клю считается истекшим)
+        if now() <= self.activation_key_expires:
+            return False
+        else:
+            return True
