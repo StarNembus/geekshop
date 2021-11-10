c3972d8b7b855cf7a65090d51890a271951c67b0
kwazart <artdd52669@gmail.com>
2021-11-10 12:08:39 +0300

    bugs fixed

--- users/forms.py
+++ users/forms.py
@@ -66,4 +66,3 @@ class UserProfileEditForm(forms.ModelForm):
         super(UserProfileEditForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-            field.help_text = ''
